513ebc01ac348215554d9ad7eba0c9b9f75d87a7
rockefelm <rockefelm@gmail.com>
2025-07-11 14:34:51 -0400

    created player class and allowed players to rotate the ship

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
-    clock = pygame.time.clock()
+    clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
             dt = clock.tick(60) / 1000
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
