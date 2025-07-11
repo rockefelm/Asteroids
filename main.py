6e672ab8e456b4ec115c3b57c5d615b508488ffb
rockefelm <rockefelm@gmail.com>
2025-07-11 13:58:25 -0400

    adding the base circleshape class and setting the fps to 60

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from constants import *
 import pygame
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            dt = clock.tick(60) / 1000
         screen.fill("black")
         pygame.display.flip()
     print("Starting Asteroids!")
