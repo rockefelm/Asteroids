9794b335c5182e0cabda217616495a4a1337ef7b
rockefelm <rockefelm@gmail.com>
2025-07-11 16:34:46 -0400

    added collision logic to shots fired

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from asteroidfield import *
 from shot import *
 
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
@@ -21,24 +22,32 @@ def main():
     Shot.containers = (shots_fired, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     initial_asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
             dt = clock.tick(60) / 1000
+
         updatable.update(dt)
+        
         for asteroid in asteroids:
             if player.collides(asteroid):
                 print("Game over!")
                 pygame.quit()
+
+        for asteroid in asteroids:
+            for shot in shots_fired:
+                if asteroid.collides(shot):
+                    asteroid.kill()
+                    shot.kill()
+
         screen.fill("black")
         for object in drawable:
             object.draw(screen)
+
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
